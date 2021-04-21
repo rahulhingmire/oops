9e3b260f8dca73f223437870bf12f5b05924f242
Rahul Hingmire <67729354+rahulhingmire@users.noreply.github.com>
2021-04-21 14:46:13 +0530

    Create encapsulationMain.java

---
+++ encapsulationMain.java
@@ -0,0 +1,14 @@
+package oops.Package;
+
+import oops.Package.Models.*;
+
+public class main {
+	public static void main(String[] args) {
+		
+		student obj = new student("jerry");
+		
+		System.out.println(student.name);
+		
+	}
+
+}
