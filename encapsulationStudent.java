fc0945a761d9e983f011157bf7d48226edf6f8b4
Rahul Hingmire <67729354+rahulhingmire@users.noreply.github.com>
2021-04-21 14:47:49 +0530

    Create encapsulationStudent.java

---
+++ encapsulationStudent.java
@@ -0,0 +1,17 @@
+//in this we use getter and setter method but in a basic way
+package oops.Package.Models;
+
+public class student {
+	
+       public static String name;
+        
+       public student(String name){
+    	   this.name=name;
+       }
+       
+       public String getName() {
+    	   return name;
+       }
+       
+
+}
