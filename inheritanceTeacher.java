65e0fb278b4191b5c46efe51cf416a76dbc824f1
Rahul Hingmire <67729354+rahulhingmire@users.noreply.github.com>
2021-04-22 13:51:24 +0530

    Create inheritanceTeacher.java

---
+++ inheritanceTeacher.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class teacher extends person {
+	
+	public void teach() {
+		System.out.println("teacher "+name+" teaching. ");
+	}
+	
+	public void eat() {
+		System.out.println("teacher "+name+" eating. ");
+	}
+
+}
