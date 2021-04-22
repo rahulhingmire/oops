e561d66770a73649077046eacfd6db908615f540
Rahul Hingmire <67729354+rahulhingmire@users.noreply.github.com>
2021-04-22 13:50:46 +0530

    Create inheritancePerson.java

---
+++ inheritancePerson.java
@@ -0,0 +1,19 @@
+package inheritance;
+
+public class person {
+
+		protected String name;
+		
+		public void eat() {
+			System.out.println("person "+name+" eating. ");
+		}
+		
+		public void walk() {
+			System.out.println("person "+name+" eating. ");
+		}
+		
+		public void laugh() {
+			System.out.println("person "+name+" laughing ");
+		}
+
+}
