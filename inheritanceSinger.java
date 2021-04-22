1a5bf69a63d565db149a74cb1cbeb88de196ab47
Rahul Hingmire <67729354+rahulhingmire@users.noreply.github.com>
2021-04-22 13:52:07 +0530

    Create inheritanceSinger.java

---
+++ inheritanceSinger.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class singer extends person {
+	
+	public void singer() {
+		System.out.println("singer "+name+" singing ");
+	}
+	
+	public void eat() {
+		System.out.println("singer "+name+" eating. ");
+	}
+
+}
